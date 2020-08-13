05b330b43f4ced03aa2ea0cda5d1403eafcff8b8
betyquiro <betyquiro@gmail.com>
2020-08-13 20:30:14 -0300

    primer import

---
+++ app.js
@@ -0,0 +1,9 @@
+export function validate(){
+    var username = document.getElementById("username").value;
+    var password= document.getElementById("password").value;
+    if (username == 'pepe' && password == 'pepe'){
+        window.location = "./views/resultado_ok.html";
+    } else {
+        window.location = "./views/resultado_error.html";
+    }
+ }
